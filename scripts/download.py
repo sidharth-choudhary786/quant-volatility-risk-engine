257d1f2cceb285ba6307d021eac22413e956292a
Sidharth Choudhary <174858061+sidharth-choudhary786@users.noreply.github.com>
2025-12-27 17:02:50 +0530

    Add imports and package installation comment
    Added import statements and installation comment for required packages.

--- scripts/download.py
+++ scripts/download.py
@@ -1,1 +1,9 @@
+# !pip install arch yfinance pandas numpy matplotlib tqdm pyarrow
 
+import os
+import time
+from datetime import datetime, timedelta
+import pandas as pd
+import numpy as np
+import yfinance as yf
+from tqdm import tqdm
